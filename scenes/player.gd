9b2a54368f5d32f2183f24d97403999a882140f3
Ionut-Valeriu <ionut.valeriu12@gmail.com>
2025-04-05 12:15:32 +0300

    skeleton phaze 1

---
+++ scenes/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@onready var color_rect = $ColorRect as ColorRect
+@onready var animation_player = $AnimationPlayer as AnimationPlayer
+
+@export var move_speed : float = 150.0
+@export var attack_speed : float = 400.0
+@export var movement_direction : Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	handle_movement()
+	
+	move_and_slide()
+
+func handle_movement() -> void:
+	var movement_x : float = Input.get_axis("move_left", "move_right")
+	var movement_y : float = Input.get_axis("move_up", "move_down")
+	
+	movement_direction = Vector2(movement_x, movement_y).normalized()
+	
+	velocity = movement_direction * move_speed
+	
+	print("X: ", movement_x, " Y: ", movement_y)
+	
+	#animatii
